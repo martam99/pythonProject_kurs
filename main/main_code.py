db190c849d54285c300e8b4696065d0e80f422a0
Mariam Tamrazyan <mart76901@gmail.com>
2023-05-16 10:53:24 +0400

    commit final project

--- main/main_code.py
+++ main/main_code.py
@@ -1,15 +1,21 @@
-from functions import main
+from functions import get_json_list, get_executed_status, sorted_data, get_date, get_card_number, get_sent_number
 from pprint import pprint
 
 if __name__ == "__main__":
-    def final1_list(final=main()):
-        for el in final:
-            try:
+    def final1_list():
+        file_name = get_json_list('file.json')
+        executed = get_executed_status(file_name)
+        sorted_l = sorted_data(executed)
+        date = get_date(sorted_l)
+        card_num = get_card_number(date)
+        sent_num = get_sent_number(card_num)
+        for el in sent_num:
+            if 'from' in el:
                 print(f"{el['date']} {el['description']}")
                 print(f"{el['from']}--->{el['to']}")
                 print(f"{el['operationAmount']['amount']}  {el['operationAmount']['currency']['name']}")
                 print("")
-            except KeyError:
+            else:
                 print(f"{el['date']} {el['description']}")
                 print(f"{None}--->{el['to']}")
                 print(f"{el['operationAmount']['amount']}  {el['operationAmount']['currency']['name']}")
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,9 +1,9 @@
-from main.functions import main, get_executed_status, sorted_data, get_card_number, get_date, get_sent_number
+from main.functions import get_executed_status, sorted_data, get_card_number, get_date, get_sent_number
 import pytest
 
 
 @pytest.fixture
-def compare():
+def transactions():
     return [{
         "id": 27192367,
         "state": "CANCELED",
@@ -33,56 +33,11 @@ def compare():
             "description": "Перевод организации",
             "from": "Счет 26406253703545413262",
             "to": "Счет 20735820461482021315"
-        },
-        {
-            "id": 207126257,
-            "state": "EXECUTED",
-            "date": "2019-07-15T11:47:40.496961",
-            "operationAmount": {
-                "amount": "92688.46",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Открытие вклада",
-            "to": "Счет 35737585785074382265"
-        },
-        {
-            "id": 957763565,
-            "state": "EXECUTED",
-            "date": "2019-01-05T00:52:30.108534",
-            "operationAmount": {
-                "amount": "87941.37",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 46363668439560358409",
-            "to": "Счет 18889008294666828266"
-        },
-        {
-            "id": 667307132,
-            "state": "EXECUTED",
-            "date": "2019-07-13T18:51:29.313309",
-            "operationAmount": {
-                "amount": "97853.86",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод с карты на счет",
-            "from": "Maestro 1308795367077170",
-            "to": "Счет 96527012349577388612"
-        }
-    ]
+        }]
 
 
-def test_get_executed_status(compare):
-    assert get_executed_status(compare) == [
+def test_get_executed_status(transactions):
+    assert get_executed_status(transactions) == [
         {
             "id": 921286598,
             "state": "EXECUTED",
@@ -97,100 +52,11 @@ def test_get_executed_status(compare):
             "description": "Перевод организации",
             "from": "Счет 26406253703545413262",
             "to": "Счет 20735820461482021315"
-        },
-        {
-            "id": 207126257,
-            "state": "EXECUTED",
-            "date": "2019-07-15T11:47:40.496961",
-            "operationAmount": {
-                "amount": "92688.46",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Открытие вклада",
-            "to": "Счет 35737585785074382265"
-        },
-        {
-            "id": 957763565,
-            "state": "EXECUTED",
-            "date": "2019-01-05T00:52:30.108534",
-            "operationAmount": {
-                "amount": "87941.37",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 46363668439560358409",
-            "to": "Счет 18889008294666828266"
-        },
-        {
-            "id": 667307132,
-            "state": "EXECUTED",
-            "date": "2019-07-13T18:51:29.313309",
-            "operationAmount": {
-                "amount": "97853.86",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод с карты на счет",
-            "from": "Maestro 1308795367077170",
-            "to": "Счет 96527012349577388612"
-        }
-    ]
+        }]
 
 
-def test_sorted_data(compare):
-    assert sorted_data(compare) == [
-        {
-            "id": 207126257,
-            "state": "EXECUTED",
-            "date": "2019-07-15T11:47:40.496961",
-            "operationAmount": {
-                "amount": "92688.46",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Открытие вклада",
-            "to": "Счет 35737585785074382265"
-        },
-        {
-            "id": 667307132,
-            "state": "EXECUTED",
-            "date": "2019-07-13T18:51:29.313309",
-            "operationAmount": {
-                "amount": "97853.86",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод с карты на счет",
-            "from": "Maestro 1308795367077170",
-            "to": "Счет 96527012349577388612"
-        },
-        {
-            "id": 957763565,
-            "state": "EXECUTED",
-            "date": "2019-01-05T00:52:30.108534",
-            "operationAmount": {
-                "amount": "87941.37",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 46363668439560358409",
-            "to": "Счет 18889008294666828266"
-        },
+def test_sorted_data(transactions):
+    assert sorted_data(transactions) == [
         {
             "id": 27192367,
             "state": "CANCELED",
@@ -224,8 +90,8 @@ def test_sorted_data(compare):
     ]
 
 
-def test_get_date(compare):
-    assert get_date(compare) == [{
+def test_get_date(transactions):
+    assert get_date(transactions) == [{
         "id": 27192367,
         "state": "CANCELED",
         "date": "24.12.2018",
@@ -254,56 +120,11 @@ def test_get_date(compare):
             "description": "Перевод организации",
             "from": "Счет 26406253703545413262",
             "to": "Счет 20735820461482021315"
-        },
-        {
-            "id": 207126257,
-            "state": "EXECUTED",
-            "date": "15.07.2019",
-            "operationAmount": {
-                "amount": "92688.46",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Открытие вклада",
-            "to": "Счет 35737585785074382265"
-        },
-        {
-            "id": 957763565,
-            "state": "EXECUTED",
-            "date": "05.01.2019",
-            "operationAmount": {
-                "amount": "87941.37",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 46363668439560358409",
-            "to": "Счет 18889008294666828266"
-        },
-        {
-            "id": 667307132,
-            "state": "EXECUTED",
-            "date": "13.07.2019",
-            "operationAmount": {
-                "amount": "97853.86",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод с карты на счет",
-            "from": "Maestro 1308795367077170",
-            "to": "Счет 96527012349577388612"
-        }
-    ]
+        }]
 
 
-def test_card_number(compare):
-    assert get_card_number(compare) == [
+def test_card_number(transactions):
+    assert get_card_number(transactions) == [
       {
           "id": 27192367,
           "state": "CANCELED",
@@ -333,57 +154,11 @@ def test_card_number(compare):
             "description": "Перевод организации",
             "from": "Счет 2640 62** **** **** 3262",
             "to": "Счет 20735820461482021315"
-        },
-        {
-            "id": 207126257,
-            "state": "EXECUTED",
-            "date": "2019-07-15T11:47:40.496961",
-            "operationAmount": {
-                "amount": "92688.46",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Открытие вклада",
-            "to": "Счет 35737585785074382265"
-        },
-        {
-            "id": 957763565,
-            "state": "EXECUTED",
-            "date": "2019-01-05T00:52:30.108534",
-            "operationAmount": {
-                "amount": "87941.37",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 4636 36** **** **** 8409",
-            "to": "Счет 18889008294666828266"
-        },
-        {
-            "id": 667307132,
-            "state": "EXECUTED",
-            "date": "2019-07-13T18:51:29.313309",
-            "operationAmount": {
-                "amount": "97853.86",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод с карты на счет",
-            "from": "Maestro 1308 79** **** 7170",
-            "to": "Счет 96527012349577388612"
-        }
-
-    ]
+        }]
 
 
-def test_sent_number(compare):
-    assert get_sent_number(compare) == [{
+def test_sent_number(transactions):
+    assert get_sent_number(transactions) == [{
         "id": 27192367,
         "state": "CANCELED",
         "date": "2018-12-24T20:16:18.819037",
@@ -412,49 +187,4 @@ def test_sent_number(compare):
             "description": "Перевод организации",
             "from": "Счет 26406253703545413262",
             "to": "Счет **1315"
-        },
-        {
-            "id": 207126257,
-            "state": "EXECUTED",
-            "date": "2019-07-15T11:47:40.496961",
-            "operationAmount": {
-                "amount": "92688.46",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Открытие вклада",
-            "to": "Счет **2265"
-        },
-        {
-            "id": 957763565,
-            "state": "EXECUTED",
-            "date": "2019-01-05T00:52:30.108534",
-            "operationAmount": {
-                "amount": "87941.37",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 46363668439560358409",
-            "to": "Счет **8266"
-        },
-        {
-            "id": 667307132,
-            "state": "EXECUTED",
-            "date": "2019-07-13T18:51:29.313309",
-            "operationAmount": {
-                "amount": "97853.86",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод с карты на счет",
-            "from": "Maestro 1308795367077170",
-            "to": "Счет **8612"
-        }
-    ]
+        }]
